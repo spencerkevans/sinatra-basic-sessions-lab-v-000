74b8f1152d46d22d6c1dcc33d2116b11bcf2af6c
spensation <spencevans@gmail.com>
2020-02-26 14:24:42 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	enable :sessions
+
+	get '/' do 
+		erb :index
+	end
+
+	post '/checkout' do 
+		session[:item] = params[:item]
+		
+		"there is a(n) #{session[:item]} in your cart"
+	end
 end
